1a9ebec145cd9ea8b3945213cd0bcf3320fa7419
Uzi Karmeli <karmeli@Uzi-MacBook-Pro.local>
2024-04-05 22:06:19 +0300

    finish pet filtering

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ async function petsArea() {
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
 
+    clone.querySelector(".pet-card").dataset.species = pet.species;
     clone.querySelector("h3").textContent = pet.name;
     clone.querySelector(".pet-description").textContent = pet.description;
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
@@ -46,4 +47,25 @@ function createAgeText(birthYear) {
   if (age == 0) return "Less than a year old";
 
   return `${age} years old`;
+}
+
+const allButtons = document.querySelectorAll(".pet-filter button");
+
+allButtons.forEach((el) => {
+  el.addEventListener("click", handleButtonClick);
+});
+
+function handleButtonClick(e) {
+  allButtons.forEach((el) => el.classList.remove("active"));
+
+  e.target.classList.add("active");
+
+  const currentFilter = e.target.dataset.filter;
+  document.querySelectorAll(".pet-card").forEach((el) => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid";
+    } else {
+      el.style.display = "none";
+    }
+  });
 }
