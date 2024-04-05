ee29040ef03f7d934d766ae07ba59fa8df21a490
Uzi Karmeli <karmeli@Uzi-MacBook-Pro.local>
2024-04-05 22:08:06 +0300

    update main JS

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const template = document.querySelector("#pet-card-template");
 const wrapper = document.createDocumentFragment();
 
-// async function start() {
-//   const weatherPromise = await fetch(
-//     "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
-//   );
-//   const weatherData = await weatherPromise.json();
-//   temperature = weatherData.properties.periods[0].temperature;
-//   document.querySelector("#temperature-output").textContent = temperature;
-// }
+async function start() {
+  const weatherPromise = await fetch(
+    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+  );
+  const weatherData = await weatherPromise.json();
+  temperature = weatherData.properties.periods[0].temperature;
+  document.querySelector("#temperature-output").textContent = temperature;
+}
 
-//start();
+start();
 
 async function petsArea() {
   const petsPromise = await fetch(
