4509cb6c7facba9405ee570954762fe3f75e4afb
Uzi Karmeli <karmeli@Uzi-MacBook-Pro.local>
2024-04-05 11:21:02 +0300

    add fetch pets data

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-async function start() {
-  const weatherPromise = await fetch(
-    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+// async function start() {
+//   const weatherPromise = await fetch(
+//     "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+//   );
+//   const weatherData = await weatherPromise.json();
+//   temperature = weatherData.properties.periods[0].temperature;
+//   document.querySelector("#temperature-output").textContent = temperature;
+// }
+
+//start();
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
   );
-  const weatherData = await weatherPromise.json();
-  temperature = weatherData.properties.periods[0].temperature;
-  document.querySelector("#temperature-output").textContent = temperature;
+  const petsData = await petsPromise.json();
+  petsData.forEach((pet) => {
+    console.log(pet.name);
+  });
 }
 
-start();
+petsArea();
