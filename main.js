fad5405eef6addfff08c7dad3e65e9938c00fd0e
Uzi Karmeli <karmeli@Uzi-MacBook-Pro.local>
2024-04-11 22:38:42 +0300

    update source for pets list

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const template = document.querySelector("#pet-card-template");
 const wrapper = document.createDocumentFragment();
 
 async function start() {
-  const weatherPromise = await fetch(
-    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
-  );
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
   const weatherData = await weatherPromise.json();
   temperature = weatherData.properties.periods[0].temperature;
   document.querySelector("#temperature-output").textContent = temperature;
@@ -13,9 +11,7 @@ async function start() {
 start();
 
 async function petsArea() {
-  const petsPromise = await fetch(
-    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
-  );
+  const petsPromise = await fetch("https://master--dreamy-seahorse-e59a62.netlify.app/.netlify/functions/pets");
   const petsData = await petsPromise.json();
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
@@ -28,9 +24,7 @@ async function petsArea() {
     if (!pet.photo) pet.photo = "images/fallback.jpg";
 
     clone.querySelector(".pet-card-photo img").src = pet.photo;
-    clone.querySelector(
-      ".pet-card-photo img"
-    ).alt = `A ${pet.species} named ${pet.name}`;
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`;
 
     wrapper.appendChild(clone);
   });
